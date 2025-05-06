bf67f1cebba719d5f68b2c5aedee77a79081d747
regionliu <1070484884@qq.com>
2025-05-06 10:57:43 +0800

    本地开发使用本地mongo

--- database.py
+++ database.py
@@ -2,14 +2,15 @@
 from pymongo import MongoClient
 import certifi
 # 启用异步客户端配置（取消注释）
-MONGO_URI = "mongodb+srv://liuzixin0418:lzx990418@deepseek.kfpqcch.mongodb.net/?retryWrites=true&w=majority&appName=deepseek"
+
+#本地mongo启动 mongdo
+# brew services start mongodb-community@8.0
+MONGO_URI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.0"
 DATABASE_NAME = "deepseek" 
 
 # 同步客户端（用于初始化）
 uri = MONGO_URI
-client = MongoClient(uri,
-                     tls=True,
-                     tlsCAFile=certifi.where())
+client = MongoClient(uri)
 
 db = client[DATABASE_NAME]
 
