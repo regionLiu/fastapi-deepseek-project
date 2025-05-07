c19cf466009659c4d682531a380d3438f97d967f
regionliu <1070484884@qq.com>
2025-05-07 20:32:52 +0800

    移除mongo uri 全部转成环境变量

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
-
 from pymongo import MongoClient
 import certifi
-# 启用异步客户端配置（取消注释）
+import yaml
+import os
+from config.config import get_config
+# 读取配置文件
 
-#本地mongo启动 mongdo
-# brew services start mongodb-community@8.0
-MONGO_URI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.0"
-DATABASE_NAME = "deepseek" 
+config = get_config("mongo")
+
+# 从配置文件获取MongoDB配置
+MONGO_URI = config["mongo_uri"]
+DATABASE_NAME = "deepseek"
 
 # 同步客户端（用于初始化）
-uri = MONGO_URI
-client = MongoClient(uri)
-
+client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
-
